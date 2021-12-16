028039cb7cea69b255b1d0f8f327ac7d1fb089f6
Abhishek9267 <91148557+Abhishek9267k@users.noreply.github.com>
2021-12-16 14:45:15 -0500

    Challenge 5-input form and Firestore saving complete

--- app/src/main/java/com/gc200424380/abhishekkanojia200424380final/AddActivity.kt
+++ app/src/main/java/com/gc200424380/abhishekkanojia200424380final/AddActivity.kt
@@ -2,10 +2,39 @@ package com.gc200424380.abhishekkanojia200424380final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gc200424380.abhishekkanojia200424380final.databinding.ActivityAddBinding
+import com.gc200424380.abhishekkanojia200424380final.databinding.ActivityRegisterBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAddDest.setOnClickListener {
+            if(binding.txtDestName.text.toString().isNotEmpty() &&
+                binding.txtDestDesc.text.toString().isNotEmpty() &&
+                binding.spRanking.selectedItemPosition > 0){
+
+                val destination = Destination()
+                destination.Name = binding.txtDestName.text.toString()
+                destination.Desc = binding.txtDestDesc.text.toString()
+                destination.Rank = binding.spRanking.selectedItem.toString().toInt()
+
+                val db = FirebaseFirestore.getInstance().collection("Destinations")
+                destination.id = db.document().id
+
+                db.document(destination.id!!).set(destination)
+
+            }else {
+                Toast.makeText(this, "Destination name, description and ranking required", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/gc200424380/abhishekkanojia200424380final/Destination.kt
@@ -0,0 +1,3 @@
+package com.gc200424380.abhishekkanojia200424380final
+
+class Destination(var id: String?=null,var Name : String? = null,var Desc : String? = null,var Rank : Int? = null) {}
